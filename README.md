a18e16269516237e374840c0649c5903391c7447
J. Cohen <hostelmarketing@gmail.com>
2021-06-15 03:34:27 -0700

    Add more code and notes

---
+++ README.md
@@ -0,0 +1,6 @@
+Some experiments and notes based on [this](https://rust-cli.github.io/book/index.html).
+
+```text
+$ RUST_LOG=info cargo run -- main src/main.rs
+$ cargo build --release
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+// env_logger is the logging adapter in this project, but there are more
+// listed here:
+// https://docs.rs/log/0.4.14/log/
+// env_logger looks for an env var like: `RUST_LOG=info`
+use log::{info, warn};
+
 // colors
 // https://docs.rs/ansi_term/0.9.0/ansi_term/
 use ansi_term::Colour::Cyan;
 
-use indicatif::ProgressBar;
 // https://docs.rs/indicatif/0.16.2/indicatif/
-
+use indicatif::ProgressBar;
 
 // custom errors, for example:
 // let content = std::fs::read_to_string(path)
@@ -29,6 +34,9 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
+    env_logger::init();
+    info!("booting application");
+    warn!("testing a warn message");
     let stdout = io::stdout();
     // let mut handle = io::BufWriter::new(stdout);
     // locking here prevents the system from locking and unlocking over and over
@@ -43,7 +51,7 @@ fn main() -> Result<()> {
     let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
     let bar = ProgressBar::new(20000);
-    for _ in 0..20000{
+    for _ in 0..20000 {
         bar.inc(1);
     }
     bar.finish();
